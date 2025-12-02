171f9e6b79461b943a8394b4b0a3c47a2a5f54d0
684244141-cmyk <684244141@mail.pbru.ac.th>
2025-12-02 14:52:21 +0700

    Add files via upload

---
+++ Lab1-2.cpp
@@ -0,0 +1,19 @@
+// Factorial of n = 1*2*3*...*n
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}
+int main() {
+    int n = 5;
+    int result = factorial(n);
+    cout << "Factorial of " << n << " is " << result << endl;
+
+    return 0;
+}
